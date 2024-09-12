180e18765bc2614e4fbddd8eab0cb9fa66d23aa7
Suraiya Mahmuda <jucse29.364@gmail.com>
2024-09-12 15:55:57 +0600

    eddit

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -25,16 +25,17 @@ namespace CardGames
 
 			// Draw the top card
 			Card top = myGame.TopCard;
+			
+			else
+			{
+				SwinGame.DrawText ("No card played yet...", Color.RoyalBlue, 0, 20);
+			}
 			if (top != null)
 			{
 				SwinGame.DrawText ("Top Card is " + top.ToString (), Color.RoyalBlue, 0, 20);
 				SwinGame.DrawText ("Player 1 score: " + myGame.Score(0), Color.RoyalBlue, 0, 30);
 				SwinGame.DrawText ("Player 2 score: " + myGame.Score(1), Color.RoyalBlue, 0, 40);
 				SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"), top.CardIndex, 350, 50);
-			}
-			else
-			{
-				SwinGame.DrawText ("No card played yet...", Color.RoyalBlue, 0, 20);
 			}
 
 			// Draw the back of the cards... to represent the deck
